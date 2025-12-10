2587073ebcda278016e2374cc0800951306ebed8
José Campelo <josecampelojr@gmail.com>
2025-12-10 05:03:38 -0300

    feat: implementa ApplicationDbContext para acesso a dados
    - Cria a classe de contexto herdando do Entity Framework Core na pasta Data.
    - Configura o DbSet de Despesas para mapear a tabela no banco de dados.
    - Adiciona construtor para receber as opções de configuração via injeção de dependência.

---
+++ ControleMensalDespesas.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ControleMensalDespesas.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleMensalDespesas.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Despesa> Despesas { get; set; }
+}
