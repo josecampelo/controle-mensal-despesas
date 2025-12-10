a703e128bcb88bae14fb6840a28da0df7a76707b
José Campelo <josecampelojr@gmail.com>
2025-12-10 05:50:07 -0300

    db: gera migration Inicial para criação da tabela Despesas
    - Gera o código de migração (Up/Down) baseado na entidade Despesa.
    - Define a estrutura da tabela com colunas tipadas (decimal, nvarchar, bit).
    - Configura a chave primária (PK) e restrições de nulidade conforme Data Annotations.

---
+++ ControleMensalDespesas.Web/Data/Migrations/20251210083239_Inicial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleMensalDespesas.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Despesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Categoria = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    EstaPago = table.Column<bool>(type: "bit", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ParcelaAtual = table.Column<int>(type: "int", nullable: true),
+                    TotalParcelas = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Despesas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Despesas");
+        }
+    }
+}
